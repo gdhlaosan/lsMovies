4e509a8bf4567d9791014e51b94d02ecd4aded7e
“gaolaosan <l>
2017-06-15 16:30:42 +0800

    新建的短评组件

---
+++ src/components/Comments.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+     {{passData}}
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import jsonp from 'jsonp'
+export default {
+  props:["passData"],
+  name: 'Comments',
+  created(){
+    var url = "https://m.douban.com/rexxar/api/v2/movie/20451290/interests"
+    jsonp(url,null,(err,data)=>{
+      if(err){
+        console.error(err.message);
+      }else{
+       console.log(data)
+       this.obj = data;
+       //console.log(this.obj)  
+      }
+    })
+  }
+}
+
+</script>
+
+<style>
+
+</style>
