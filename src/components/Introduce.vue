9d0c65d357b955dd3bfcf724b56b90d1750d5e79
4191 <419135619@q.com>
2017-06-16 18:13:17 +0800

    nothing

--- src/components/Introduce.vue
+++ src/components/Introduce.vue
@@ -20,6 +20,14 @@ export default {
       p.style.height = "auto";
       p.children[1].style.display = "none"
     }
+  },
+  created(){
+    // 注册
+    Vue.filter('my-filter', function (value) {
+      // 返回处理后的值
+    })
+    // getter，返回已注册的过滤器
+    var myFilter = Vue.filter('my-filter')
   }
 }
 
