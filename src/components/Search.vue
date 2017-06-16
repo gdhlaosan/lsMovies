e66b244ba2fa9d904f812a80f4df72ca0dd991fb
“gaolaosan <l>
2017-06-16 18:16:00 +0800

    还没有完成模糊查询

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,20 +1,42 @@
 <template>
   <div id="search">
   	<div id="SearchBox">
-	  	<mt-search v-model="value" cancel-text="取消" placeholder="搜索" :result.sync="result"></mt-search>
+	  	<mt-search :value.sync="value" cancel-text="取消" placeholder="搜索">
+      </mt-search>
   	</div>
+   <!--  <button @click="get">炸了</button>  -->
   </div>
 </template>
 
 <script>
+import Vue from 'vue'
+import jsonp from 'jsonp'
 export default {
   name: 'Search',
   data(){
   	return {
-  		value:""
+  		result:"",
+      value:""
   	}
   },
-  
+  updated(){
+    //var url = "../../static/search.json"
+    //var url = "https://api.douban.com/v2/movie/search?q="+this.value
+    // Vue.axios.get(url).then((res) => {
+    //   return res.data.subjects
+    // }).then((data)=>{
+    //   this.result = data;
+    //   console.log(this.result)
+    // })
+    // jsonp(url,null,(err,data)=>{
+    //   if(err){
+    //     console.error(err.message);
+    //   }else{
+    //    console.log(data)
+    //    this.result = data.subjects; 
+    //   }
+    // })
+  }
 }
 </script>
 
